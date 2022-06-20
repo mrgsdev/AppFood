cf98111bff562d3948fd509e359d5b2d559c824d
mrgsdev <mrgsdev@gmail.com>
2022-06-20 10:50:51 +0300

    WKWebView and SFSafariViewController

--- AppFood/Controller/AboutTableTableViewController.swift
+++ AppFood/Controller/AboutTableTableViewController.swift
@@ -6,11 +6,13 @@
 //
 
 import UIKit
-
+import SafariServices
 class AboutTableTableViewController: UITableViewController {
+    
     enum Section {
         case feedback
         case followus
+        case versionapp
     }
     struct LinkItem:Hashable {
         var text:String
@@ -20,9 +22,10 @@ class AboutTableTableViewController: UITableViewController {
     var sectionContent = [
         [LinkItem(text: "Rate us on App Store", link: "https://www.apple.com/ios/app-store/", image:"store")],
         [
-        LinkItem(text: "GitHub", link: "https://github.com/mrgsdev", image: "github"),
-        LinkItem(text: "Telegram", link: "https://teleg.run/mrgsdev", image: "telegram"),
-        ]
+            LinkItem(text: "GitHub", link: "https://github.com/mrgsdev", image: "github"),
+            LinkItem(text: "Telegram", link: "https://teleg.run/mrgsdev", image: "telegram"),
+        ],
+        [LinkItem(text: "1.0", link: "", image: "")]
     ]
     lazy var dataSource = configureDataSource()
     override func viewDidLoad() {
@@ -33,19 +36,19 @@ class AboutTableTableViewController: UITableViewController {
         updateSnapshot()
     }
     func customNavBar(){
-//        navigationController?.navigationBar.prefersLargeTitles = true
-           // Customize the navigation bar appearance
-           if let appearance = navigationController?.navigationBar.standardAppearance {
-               appearance.configureWithTransparentBackground()
-               if let customFont = UIFont(name: "Nunito-Bold", size: 20.0) {
-                   appearance.titleTextAttributes = [.foregroundColor: UIColor(named: "NavigationBarTitle")!]
-                   appearance.largeTitleTextAttributes = [.foregroundColor: UIColor(named: "NavigationBarTitle")!, .font: customFont]
-       }
-               navigationController?.navigationBar.standardAppearance = appearance
-               navigationController?.navigationBar.compactAppearance = appearance
-               navigationController?.navigationBar.scrollEdgeAppearance = appearance
-               
-           }
+        //        navigationController?.navigationBar.prefersLargeTitles = true
+        // Customize the navigation bar appearance
+        if let appearance = navigationController?.navigationBar.standardAppearance {
+            appearance.configureWithTransparentBackground()
+            if let customFont = UIFont(name: "Nunito-Bold", size: 20.0) {
+                appearance.titleTextAttributes = [.foregroundColor: UIColor(named: "NavigationBarTitle")!]
+                appearance.largeTitleTextAttributes = [.foregroundColor: UIColor(named: "NavigationBarTitle")!, .font: customFont]
+            }
+            navigationController?.navigationBar.standardAppearance = appearance
+            navigationController?.navigationBar.compactAppearance = appearance
+            navigationController?.navigationBar.scrollEdgeAppearance = appearance
+            
+        }
     }
     // MARK: - Table view diffable data source
     func configureDataSource() -> UITableViewDiffableDataSource<Section, LinkItem> {
@@ -67,65 +70,42 @@ class AboutTableTableViewController: UITableViewController {
         dataSource.apply(snapshot, animatingDifferences: false)
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        performSegue(withIdentifier: "showWebView", sender: self)
+ 
+//        if indexPath.section == 1,indexPath.row == 1 {
+//             return
+//        }
+            print(indexPath.row)
+        switch indexPath.section {
+        case 0: performSegue(withIdentifier: "showWebView", sender: self)
+        case 1: openWithSafariViewController(indexPath: indexPath)
+        default: break
+        }
         tableView.deselectRow(at: indexPath, animated: false)
-         
+        
     }
+    
+    func openWithSafariViewController(indexPath: IndexPath) {
+        guard let linkItem = self.dataSource.itemIdentifier(for: indexPath) else {
+            return
+        }
+        if let url = URL(string: linkItem.link) {
+            let safariController = SFSafariViewController(url: url)
+            present(safariController, animated: true, completion: nil)
+        }
+        
+    }
+    
+    // MARK: - Navigation
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showWebView" {
-    if let destinationController = segue.destination as? WebViewController,
-       let indexPath = tableView.indexPathForSelectedRow,
-       let linkItem = self.dataSource.itemIdentifier(for: indexPath) {
-        destinationController.targetURL = linkItem.link
-    }
+            if let destinationController = segue.destination as? WebViewController,
+               let indexPath = tableView.indexPathForSelectedRow,
+               let linkItem = self.dataSource.itemIdentifier(for: indexPath) {
+                destinationController.targetURL = linkItem.link
+            }
             
         }
     }
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
     
 }
